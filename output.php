033b2b3d95285e5663f93f0c1dc0bace50b1c5cf
suzan <asadsuzan7@gmail.com>
2025-07-24 13:04:31 +0600

    doc: php print statement

--- output.php
+++ output.php
@@ -21,7 +21,7 @@
     
 
           //   php echo statement | the echo statement can be used with or without parentheses 
-
+ echo "<h1>The PHP echo Statement</h1>";
   echo "hello world without parentheses <br>";
   echo ("hello world with parentheses <br>");
 
@@ -50,7 +50,50 @@
 
  $name="asad";
  $age=25;
- echo '<p>my name is '. $name . ' and im '. $age. ' years old</p>'
+ echo '<p>my name is '. $name . ' and im '. $age. ' years old</p> <br>';
+
+
+
+// The PHP print Statement
+// the print statement can be used with or without parentheses 
+ echo "<h1>The PHP print Statement</h1>";
+print "hello without parentheses <br>";
+//same as 
+print("hello with parentheses <br>");
+
+  // display text 
+  echo "<h1>Display text</h1>";
+
+
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!<br>";
+
+$txt12 = "Learn PHP";
+$txt21 = "W3Schools.com";
+
+print "<h2>$$txt12</h2>";
+print "<p>Study PHP at $txt21</p>";
+
+
+/*
+
+sing Single Quotes
+Strings are surrounded by quotes, but there is a difference between single and double quotes in PHP.
+
+When using double quotes, variables can be inserted to the string as in the example above.
+
+When using single quotes, variables have to be inserted using the . operator, like this:
+
+*/
+
+
+
+$txt133= "Learn PHP";
+$txt211 ="W3Schools.com";
+
+print '<h2>' . $txt133 . '</h2>';
+print '<p>Study PHP at ' . $txt211 . '</p>';
     ?>
 </body>
 </html>
