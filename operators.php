fb14ea76c76dd3440660a8cef94702d24019a291
suzan <asadsuzan7@gmail.com>
2025-07-26 14:38:36 +0600

    doc: php operators

---
+++ operators.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php operators</title>
+</head>
+<body>
+   <h4> PHP Operators</h4>
+<p>Operators are used to perform operations on variables and values.
+</p>
+<p>PHP divides the operators in the following groups:</p>
+<ol>
+    <li>Arithmetic operators</li>
+    <li>Assignment operators</li>
+    <li>Comparison operators</li>
+    <li>Increment/Decrement operators</li>
+    <li>Logical operators</li>
+    <li>String operators</li>
+    <li>Array Operators</li>
+    <li>Conditional Assignment operators</li>
+</ol>
+
+<?php
+// arithmetic operators
+
+echo "<h4>PHP Arithmetic Operators</h4>";
+echo "<p>Addition (+)</p>";
+
+$x = 10;  
+$y = 6;
+
+echo $x + $y;
+echo "<br>";
+
+echo "<p>Subtraction (-)</p>";
+$x = 10;  
+$y = 6;
+
+echo $x - $y;
+echo "<p>multiplication (*)</p>";
+$x = 10;  
+$y = 6;
+
+echo $x * $y;
+echo "<br>";
+echo "<p>division (/)</p>";
+$x = 10;  
+$y = 6;
+
+echo $x / $y;
+echo "<br>";
+
+echo "<p>modulus (%)</p>";
+$x = 10;  
+$y = 6;
+
+echo $x % $y;
+echo "<br>";
+echo "<p>exponentiation (**)</p>";
+$x = 10;  
+$y = 6;
+
+echo $x ** $y;
+echo "<br>";
+
+
+
+
+?>
+
+
+
+
+
+</body>
+</html>
